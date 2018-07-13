85058d8a5ffb053b3bca6a64e3da8542c259206c
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2018-07-13 17:31:51 +0300

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,113 @@
+'use strict';
+
+const del = require(`del`);
+const gulp = require(`gulp`);
+const sass = require(`gulp-sass`);
+const plumber = require(`gulp-plumber`);
+const postcss = require(`gulp-postcss`);
+const autoprefixer = require(`autoprefixer`);
+const server = require(`browser-sync`).create();
+const mqpacker = require(`css-mqpacker`);
+const minify = require(`gulp-csso`);
+const rename = require(`gulp-rename`);
+const imagemin = require(`gulp-imagemin`);
+const svgstore = require(`gulp-svgstore`);
+
+gulp.task(`style`, () => {
+  return gulp.src(`sass/style.scss`).
+    pipe(plumber()).
+    pipe(sass()).
+    pipe(postcss([
+      autoprefixer({
+        browsers: [
+          `last 1 version`,
+          `last 2 Chrome versions`,
+          `last 2 Firefox versions`,
+          `last 2 Opera versions`,
+          `last 2 Edge versions`
+        ]
+      }),
+      mqpacker({sort: true})
+    ])).
+    pipe(gulp.dest(`build/css`)).
+    pipe(server.stream()).
+    pipe(minify()).
+    pipe(rename(`style.min.css`)).
+    pipe(gulp.dest(`build/css`));
+});
+
+gulp.task(`sprite`, () => {
+  return gulp.src(`img/sprite/*.svg`)
+  .pipe(svgstore({
+    inlineSvg: true
+  }))
+  .pipe(rename(`sprite.svg`))
+  .pipe(gulp.dest(`build/img`));
+});
+
+gulp.task(`scripts`, () => {
+  return gulp.src(`js/**/*.js`).
+    pipe(plumber()).
+    pipe(gulp.dest(`build/js/`));
+});
+
+gulp.task(`imagemin`, [`copy`], () => {
+  return gulp.src(`build/img/**/*.{jpg,png,gif}`).
+    pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true})
+    ])).
+    pipe(gulp.dest(`build/img`));
+});
+
+gulp.task(`copy-html`, () => {
+  return gulp.src(`*.{html,ico}`).
+    pipe(gulp.dest(`build`)).
+    pipe(server.stream());
+});
+
+gulp.task(`copy`, [`copy-html`, `scripts`, `style`, `sprite`], () => {
+  return gulp.src([
+    `fonts/**/*.{woff,woff2}`,
+    `img/*.*`
+  ], {base: `.`}).
+    pipe(gulp.dest(`build`));
+});
+
+gulp.task(`clean`, () => {
+  return del(`build`);
+});
+
+gulp.task(`js-watch`, [`scripts`], (done) => {
+  server.reload();
+  done();
+});
+
+gulp.task(`serve`, [`assemble`], () => {
+  server.init({
+    server: `./build`,
+    notify: false,
+    open: true,
+    port: 3502,
+    ui: false
+  });
+
+  gulp.watch(`sass/**/*.{scss,sass}`, [`style`]);
+  gulp.watch(`*.html`).on(`change`, (e) => {
+    if (e.type !== `deleted`) {
+      gulp.start(`copy-html`);
+    }
+  });
+  gulp.watch(`js/**/*.js`, [`js-watch`]);
+});
+
+gulp.task(`assemble`, [`clean`], () => {
+  gulp.start(`copy`, `style`);
+});
+
+gulp.task(`build`, [`assemble`], () => {
+  gulp.start(`imagemin`);
+});
+
+gulp.task(`test`, () => {
+});
